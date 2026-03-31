7ee2bbdb7fd287029f38fb6e7663881f108195e4
Promise Khoza <136032125+Promisekhoza534@users.noreply.github.com>
2026-03-31 14:49:06 +0200

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using EventEase.Data;
+using EventEase.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventEase.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = new HomeViewModel
+            {
+                UpcomingEvents = await _context.Events
+                    .Include(e => e.Venue)
+                    .OrderBy(e => e.EventDate)
+                    .Take(2)
+                    .ToListAsync(),
+
+                RecentBookings = await _context.Bookings
+                    .Include(b => b.Event)
+                    .Include(b => b.Venue)
+                    .OrderByDescending(b => b.BookingDate)
+                    .Take(2)
+                    .ToListAsync()
+            };
+
+            return View(model);
+        }
+    }
+}
