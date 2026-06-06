10aebd4369d81f6573960ba6eddffcd19a9e045d
Promise Khoza <136032125+Promisekhoza534@users.noreply.github.com>
2026-06-06 19:57:00 +0200

    Update ApplicationDbContext.cs

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,8 +5,6 @@ namespace EventEase.Data
 {
     public class ApplicationDbContext : DbContext
     {
-
-
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
@@ -16,38 +14,79 @@ namespace EventEase.Data
         public DbSet<Event> Events { get; set; }
         public DbSet<Booking> Bookings { get; set; }
 
+        public DbSet<EventType> EventTypes { get; set; }
+
         public DbSet<BookingDetailsViewModel> BookingDetailsView { get; set; }
-
-
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
+          
             modelBuilder.Entity<Event>()
                 .HasOne(e => e.Venue)
                 .WithMany(v => v.Events)
                 .HasForeignKey(e => e.VenueId)
                 .OnDelete(DeleteBehavior.Restrict);
 
+          
             modelBuilder.Entity<Booking>()
                 .HasOne(b => b.Event)
                 .WithMany(e => e.Bookings)
                 .HasForeignKey(b => b.EventId)
                 .OnDelete(DeleteBehavior.Restrict);
 
+         
             modelBuilder.Entity<Booking>()
                 .HasOne(b => b.Venue)
                 .WithMany(v => v.Bookings)
                 .HasForeignKey(b => b.VenueId)
                 .OnDelete(DeleteBehavior.Restrict);
 
+           
             modelBuilder.Entity<BookingDetailsViewModel>()
                 .HasNoKey()
                 .ToView("BookingDetailsView");
+
+           
+            modelBuilder.Entity<Event>()
+                .HasOne(e => e.EventTypeNavigation)
+                .WithMany(et => et.Events)
+                .HasForeignKey(e => e.EventTypeId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<EventType>().HasData(
+                new EventType
+                {
+                    EventTypeId = 1,
+                    EventTypeName = "Conference"
+                },
+                new EventType
+                {
+                    EventTypeId = 2,
+                    EventTypeName = "Wedding"
+                },
+                new EventType
+                {
+                    EventTypeId = 3,
+                    EventTypeName = "Concert"
+                },
+                new EventType
+                {
+                    EventTypeId = 4,
+                    EventTypeName = "Workshop"
+                },
+                new EventType
+                {
+                    EventTypeId = 5,
+                    EventTypeName = "Birthday"
+                },
+                new EventType
+                {
+                    EventTypeId = 6,
+                    EventTypeName = "Corporate"
+                }
+            );
         }
     }
-
-
-    
 }
